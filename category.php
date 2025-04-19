c612cefadc916bb1953fc232fd8cc7d4f2bcc773
DrowsyExp <drowsyy.espresso@gmail.com>
2025-04-19 23:40:41 +1000

    displays the categories/sub categories in the hierarchical structure using HTML list

--- category.php
+++ category.php
@@ -7,16 +7,22 @@ $query_string = "select DISTINCT category FROM products";
 $result = mysqli_query($connection, $query_string);
 $num_rows = mysqli_num_rows($result);
 
+//display the categories and subcategories in a dropdown menu
 if ($num_rows > 0) {
+    echo '<ul>';
     foreach ($result as $category) {
-        echo "<td>" . $category['category'] . "</td>";
-        $subquery_string = "select DISTINCT sub_categories FROM products WHERE category = ".$category['category']."";
+        echo '<li>' . $category['category'];
+        //returns all distinct sub categories from products which matches with the current category
+        $subquery_string = "select DISTINCT sub_categories FROM products WHERE category = '" . $category['category'] . "'";
         $sub_result = mysqli_query($connection, $subquery_string);
         $sub_num_rows = mysqli_num_rows($sub_result);
-        foreach($sub_result as $subcategory){
-            echo "<td>" . $subcategory['sub_categories'] . "</td>";
+        foreach ($sub_result as $subcategory) {
+            echo '<ul>';
+            echo '<li>' . $subcategory['sub_categories'] . '</li>';
+            echo '</ul>';
         }
-    }    
+        echo '</li>';
+    }
+    echo '</ul>';
 }
 mysqli_close($connection);
-?>
